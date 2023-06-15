d840aa36456649c59036137c2a011b4201aaeda2
Jason <jasonel2@uci.edu>
2023-06-15 14:26:27 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#This function converts Fahrenheit to Celsius in python.
+
+def FahrenheitToCelsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5/9
+    return celsius
+
+
+
+print(FahrenheitToCelsius(32))
